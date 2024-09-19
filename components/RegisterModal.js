d91aa7fcdc073ba8a2c937a85474700b39f5c98c
Bender <ahernandez94214@gmail.com>
2024-09-19 11:38:31 -0500

    Added comments to RegisterModal

--- components/RegisterModal.js
+++ components/RegisterModal.js
@@ -4,12 +4,15 @@ import { View, TextInput, ActivityIndicator, Image, TouchableOpacity, Alert, Tex
 import { useNavigation } from '@react-navigation/native';
 import { determineGlobalStyles } from '../components/Styles';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+//Import the user class 
 import User from '../models/User';
 import Parse from 'parse/react-native.js';
+//Import this to keep track of the logged in user until they choose to log out 
 import { AuthContext } from '../context/AuthContext';
 import AsyncStorage from '@react-native-async-storage/async-storage'; 
 
 const RegisterScreen = () => {
+  //Declare all the necessary fields to create a User object 
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [username, setUsername] = useState('');
@@ -17,36 +20,39 @@ const RegisterScreen = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [email, setEmail] = useState('');
   const [phoneNumber,setPhoneNumber] = useState('');
+  //Declare any errors we may encounter when registering 
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false); 
   const navigation = useNavigation();
+  //Inherit the styling from Styles.js 
   const { styles, determinedLogo } = determineGlobalStyles();
+  //Keep track of the user once they register so they wont need to log in again 
   const { login } = useContext(AuthContext); 
-
+//Require all fields to be filled out 
   const validateForm = () => {
     if (!firstName || !lastName || !email || !phoneNumber || !password || !confirmPassword) {
       setError('All fields are required');
       return false;
-    }
+    }//Check both password and confirmPassword match 
     if (password !== confirmPassword) {
       setError('Passwords do not match');
       return false;
     }
     return true;
   };
-
+  //To not let users double register we need to check if they exist in the parse database already 
   const checkIfUserExists = async () => {
     const query = new Parse.Query(Parse.User);
     query.equalTo('username', username);
     query.equalTo('email', email);
-  
+  //Throwout appropriate error 
     try {
       const result = await query.first();
       if (result) {
         Alert.alert(
           "Registration Error",
           "User with this username or email already exists",
-          [{ text: "OK" }]
+          [{ text: "OK" }]//Button text from the alert 
         );
         return true;
       }
@@ -60,7 +66,7 @@ const RegisterScreen = () => {
       return true; 
     }
   };
-
+//If the user is not register then go ahead an register them and create a new user object 
   const handleRegister = async () => {
     if (!validateForm()) return;
 
@@ -70,7 +76,7 @@ const RegisterScreen = () => {
       setLoading(false);
       return;
     }
-
+//Create the new user object 
     const newUser = new User();
     newUser.setFirstName(firstName);
     newUser.setLastName(lastName);
@@ -79,21 +85,19 @@ const RegisterScreen = () => {
     newUser.setPassword(password);
     newUser.setEmail(email);
    
-
+//Attempt to save in the database 
     try {
       await newUser.signUp();
       setLoading(false); 
-      
-     
       const loggedInUser = await login(username, password);
       if (loggedInUser) {
        
         await AsyncStorage.setItem('sessionToken', loggedInUser.getSessionToken());
-        setTimeout(() => {
+        setTimeout(() => {//If successful then navigate to Main
           navigation.navigate('Main'); 
         }, 500);
       }
-    } catch (err) {
+    } catch (err) {//Catch the error otherwise 
       setError(err.message);
       setLoading(false); 
     }
@@ -107,7 +111,7 @@ const RegisterScreen = () => {
       extraHeight={150}
     >
       <Image source={determinedLogo} style={styles.logo} />
-      
+      {/*Here we set all the components for the input fields to make User objects */}
       <TextInput
         value={firstName}
         onChangeText={setFirstName}
@@ -128,7 +132,7 @@ const RegisterScreen = () => {
         placeholder="Phone #"
         keyboardType="phone-pad"
         style={styles.input}
-        autoCapitalize="none"
+        // autoCapitalize="none"
       />
         <TextInput
         value={email}
@@ -160,11 +164,13 @@ const RegisterScreen = () => {
       />
 
       {error && <Text style={styles.error}>{error}</Text>}
+      {/*If we are loading then here we set the indicator */}
       {loading ? (
         <View style={styles.loadingOverlay}>
           <ActivityIndicator size="large" color="#ffffff" />
         </View>
       ) : (
+        //Button to trigger the register action 
         <TouchableOpacity
           style={styles.bigButton}
           onPress={handleRegister}
